c2ef172af9b2ccee5511615a5f6b377a9ebc4277
Timur Kim <kimtimur01@gmail.com>
2022-10-13 16:57:12 +0600

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const exitHook = require('async-exit-hook');
+const app = express();
+require('express-ws')(app);
+
+const config = require('./config');
+
+const PORT = 8000;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
+
+const run = async () => {
+    await mongoose.connect(config.mongo.db, config.mongo.options);
+
+    app.listen(PORT, () => {
+        console.log(`Server started on ${PORT} port!`);
+    });
+
+    exitHook(() => {
+        mongoose.disconnect();
+        console.log('MongoDb disconnect');
+    });
+};
+
+run().catch(e => console.log(e));
---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Router} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./store/configureStore";
+import history from "./history";
+import App from './App';
+
+import './index.css';
+
+const app = (
+    <Provider store={store}>
+        <Router history={history}>
+            <App/>
+        </Router>
+    </Provider>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(app);
