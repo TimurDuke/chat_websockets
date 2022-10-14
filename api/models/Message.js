ad0be1264982fb44943b8df880f208b28a5b82c9
NMMustafina <n.m.mustafina@gmail.com>
2022-10-14 19:38:09 +0600

    added models

---
+++ api/models/Message.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const idValidator = require('mongoose-id-validator');
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    recipient: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+MessageSchema.plugin(idValidator, {message : 'Bad ID value for {PATH}'});
+const Message = mongoose.model('Message',MessageSchema);
+
+module.exports = Message;
